f28ee22fde954fb2e33553d014260687f22539dd
hpq <yuekongshui@gmail.com>
2026-04-13 14:42:41 +0800

    Initial Commit

---
+++ clipboardTests/clipboardTests.swift
@@ -0,0 +1,19 @@
+//
+//  clipboardTests.swift
+//  clipboardTests
+//
+//  Created by 水月空 on 2026/4/13.
+//
+
+import Testing
+@testable import clipboard
+
+struct clipboardTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
